1e88fd86447e5393b96b14499968acfff847e8c5
morpheus <mariageorgepapas@gmail.com>
2019-10-30 13:40:57 -0500

    Added a message

--- src/org/launchcode/Main.java
+++ src/org/launchcode/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        System.out.println("In case I don't see you, good afternoon, good evening, and good night");
     }
 }
